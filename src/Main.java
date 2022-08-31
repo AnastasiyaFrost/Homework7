ee0a2cbbe3428ad64550de79e1a8a836dd8967e9
Анастасия <grebneva.nastena@yandex.ru>
2022-08-31 11:43:39 +0300

    Готово.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Task 1
+        String firstName = "Иван ";
+        String middleName = "Иванович ";
+        String lastName = "Иванов ";
+        String fullName = firstName+middleName+lastName;
+        System.out.println("ФИО сотрудника: " + fullName);
+
+        //Task 2
+
+        String fullNameUpperCase = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника для документов: " + fullNameUpperCase);
+
+        //Task 3
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace('ё','е');
+        System.out.println(fullName);
     }
 }
